56e642462bfe0bc9c8425c34795ad4655f30fdd2
qinhaohao <hhqin@dfocuspace.com>
2020-06-01 16:28:40 +0800

    feat:实现事务控制、依赖注入

--- self-ioc-aop/src/main/java/com/study/pub/utils/ProxyFactory.java
+++ self-ioc-aop/src/main/java/com/study/pub/utils/ProxyFactory.java
@@ -1,6 +1,9 @@
 package com.study.pub.utils;
 
 import com.study.pub.pojo.Result;
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
@@ -33,6 +36,39 @@ public class ProxyFactory {
                 throw e.getCause();
             }
             return result;
+        });
+    }
+
+    /**
+     * 使用cglib动态代理生成代理对象
+     * @param obj 委托对象
+     * @return
+     */
+    public Object getCglibProxy(Object obj) {
+        return  Enhancer.create(obj.getClass(), new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                Object result = null;
+                try{
+                    // 开启事务(关闭事务的自动提交)
+                    transactionManager.beginTransaction();
+
+                    result = method.invoke(obj,objects);
+
+                    // 提交事务
+
+                    transactionManager.commitTransaction();
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    // 回滚事务
+                    transactionManager.rollbackTransaction();
+
+                    // 抛出异常便于上层servlet捕获
+                    throw e;
+
+                }
+                return result;
+            }
         });
     }
 }
